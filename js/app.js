bbdfd8ee76b2f50bb4b63131bde536a6fd476734
Emilv69 <rubendarioviveros@gmail.com>
2025-04-16 14:50:36 -0400

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,8 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    const appRoot = document.getElementById('app-root');
+    appRoot.innerHTML = `
+        <p>Sistema cargado correctamente.</p>
+        <p>Las funcionalidades completas están en desarrollo.</p>
+    `;
+});
