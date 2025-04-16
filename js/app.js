8e2bbc12ef284632dc67601532704cd4e333e43b
Emilv69 <rubendarioviveros@gmail.com>
2025-04-16 15:01:03 -0400

    Delete js directory

--- js/app.js
+++
@@ -1,8 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', () => {
-    const appRoot = document.getElementById('app-root');
-    appRoot.innerHTML = `
-        <p>Sistema cargado correctamente.</p>
-        <p>Las funcionalidades completas están en desarrollo.</p>
-    `;
-});
