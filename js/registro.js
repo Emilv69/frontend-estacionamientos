668dc318e49664d3ff7b59d58f922adb7f0d09ac
Emilv69 <rubendarioviveros@gmail.com>
2025-04-16 18:39:57 -0400

    Delete js/registro.js

--- js/registro.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const content = document.getElementById('content');
-    content.innerHTML = "<p>Contenido cargado para Registro</p>";
-});
